8f96e01d2490ce1eacefc1cf00ef2588260b7679
whatislove <v.katrich87@mail.ru>
2024-07-17 01:52:46 +0500

    Third commit: add project files

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import GameHeader from './components/GameHeader.vue'
+import GameFigure from './components/GameFigure.vue'
+import GameWrongLetters from './components/GameWrongLetters.vue'
+import GameWord from './components/GameWord.vue'
+import GamePopup from './components/GamePopup.vue'
+import GameNotification from './components/GameNotification.vue'
+import { computed, ref, watch } from 'vue'
+import { getRandomName } from './api/getRandomName'
+
+const word = ref('')
+const getRandomWord = async () => {
+  try {
+    const name = await getRandomName()
+    word.value = name.toLowerCase()
+  } catch (err) {
+    console.log(err)
+    word.value = ''
+  }
+}
+getRandomWord()
+
+const letters = ref<string[]>([])
+const correctLetters = computed(() => letters.value.filter((x) => word.value.includes(x)))
+const wrongLetters = computed(() => letters.value.filter((x) => !word.value.includes(x)))
+const isLose = computed(() => wrongLetters.value.length === 6)
+const isWin = computed(() => [...word.value].every((x) => correctLetters.value.includes(x)))
+const notification = ref<InstanceType<typeof GameNotification> | null>(null)
+const popup = ref<InstanceType<typeof GamePopup> | null>(null)
+
+watch(wrongLetters, () => {
+  if (isLose.value) {
+    popup.value?.open('lose')
+  }
+})
+
+watch(correctLetters, () => {
+  if (isWin.value) {
+    popup.value?.open('win')
+  }
+})
+
+window.addEventListener('keydown', ({ key }) => {
+  if (isLose.value || isWin.value) {
+    return
+  }
+
+  if (letters.value.includes(key)) {
+    notification.value?.open()
+    setTimeout(() => notification.value?.close(), 2000)
+    return
+  }
+
+  if (/[а-яА-ЯёЁ]/.test(key)) {
+    letters.value.push(key.toLowerCase())
+  }
+})
+
+const restart = async () => {
+  await getRandomWord()
+  letters.value = []
+  popup.value?.close()
+}
+</script>
+
+<template>
+  <GameHeader />
+  <div class="game-container">
+    <GameFigure :wrong-letters-count="wrongLetters.length" />
+    <GameWrongLetters :wrong-letters="wrongLetters" />
+    <GameWord :word="word" :correct-letters="correctLetters" />
+  </div>
+
+  <GamePopup :word="word" ref="popup" @restsrt="restart" />
+  <GameNotification ref="notification" />
+</template>
+
+<style scoped></style>
